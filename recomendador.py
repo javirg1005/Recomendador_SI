3e307cedb3919d7339fd8ea3df6f6ce5cd48fc5c
javirg1005 <55756129+javirg1005@users.noreply.github.com>
2021-05-14 11:00:40 +0200

    Progreso
    Co-Authored-By: Caco <55552731+cacolyoko@users.noreply.github.com>

--- recomendador.py
+++ recomendador.py
@@ -24,24 +24,70 @@ def insert_csv_to_table(table_name, n_col, rows):
     con.commit()
     con.close()
 
-def filtrado_usuarios(df_rate, df_movies):
+'''
+def filtrado_usuarios(df_rate, df_movies, userId):
     df_movies = df_movies.drop('genre', 1)
-    print(df_movies.head())
-    print(df_rate.head())
+    con = sqlite3.connect("Movies.db")
+    cur = con.cursor()
+    cur.execute("SELECT movieId, rating FROM ratings WHERE userId = " + str(userId))
+    userMovies = cur.fetchall()
+    con.commit()
+    con.close()
 
+    df_user_movies = pd.DataFrame(userMovies, columns=['movieId', 'rating'])
+    df_rate_movies = df_rate[~df_rate['userId'].isin([userId])]
 
-def filtrado_contenido(df_rate, df_movies):
+    #Filtrando los usuarios que han visto las películas y guardándolas
+    userSubset = df_rate_movies[df_rate_movies['movieId'].isin(df_user_movies['movieId'].tolist())]
+    print(userSubset)
+'''
+
+def filtrado_contenido(df_rate, df_movies, userId):
     #Copiando el marco de datos de la pelicula en uno nuevo ya que no necesitamos la información del género por ahora.
     df_movies_g = df_movies.copy()
+    df_movies_g['genre'] = df_movies_g.genre.str.split('|')
 
     #Para cada fila del marco de datos, iterar la lista de géneros y colocar un 1 en la columna que corresponda
-    for index, row in df_movies.iterrows():
+    for index, row in df_movies_g.iterrows():
         for g in row['genre']:
             df_movies_g.at[index, g] = 1
     #Completar los valores NaN con 0 para mostrar que una película no tiene el género de la columna
     df_movies_g = df_movies_g.fillna(0)
 
-    print(df_movies_g.head())
+    con = sqlite3.connect("Movies.db")
+    cur = con.cursor()
+    cur.execute("SELECT movieId, rating FROM ratings WHERE userId = " + str(userId))
+    userMovies = cur.fetchall()
+    con.commit()
+    con.close()
+
+    df_user_movies = pd.DataFrame(userMovies, columns=['movieId', 'rating'])
+    userMovies = df_movies_g[df_movies_g['movieId'].isin(df_user_movies['movieId'].tolist())]
+
+    #Inicializando el índice para evitar problemas a futuro
+    userMovies = userMovies.reset_index(drop=True)
+
+    #Eliminando problemas innecesarios para ahorrar memoria y evitar conflictos
+    userGenreTable = userMovies.drop('movieId', 1).drop('title', 1).drop('genre', 1).drop('year', 1)
+
+    #Producto escalar para obtener los pesos
+    userProfile = userGenreTable.transpose().dot(df_user_movies['rating'])
+
+    #Ahora llevemos los géneros de cada película al marco de datos original
+    genreTable = df_movies_g.set_index(df_movies_g['movieId'])
+
+    #Y eliminemos información innecesaria
+    genreTable = genreTable.drop('movieId', 1).drop('title', 1).drop('genre', 1).drop('year', 1)
+    
+    #Multiplicando los géneros por los pesos para luego calcular el peso promedio
+    df_reco = ((genreTable * userProfile).sum(axis=1)) / (userProfile.sum())
+    
+    #Ordena nuestra recomendación en orden descendente
+    df_reco = df_reco.sort_values(ascending=False)
+
+    #Tabla de recomendaciones final
+    df_recomendacion = df_movies.loc[df_movies['movieId'].isin(df_reco.head(20).keys())]
+    print(df_recomendacion)
 
 fill_table("links.csv", "links", 3)
 fill_table("movies.csv", "movies", 3)
@@ -55,6 +101,9 @@ cur.execute("SELECT * FROM ratings")
 ratings = cur.fetchall()
 cur.execute("SELECT * FROM movies")
 movies = cur.fetchall()
+cur.execute("SELECT userId FROM ratings GROUP BY userId")
+userListMal = cur.fetchall()
+userList = [item for elem in userListMal for item in elem]
 con.commit()
 con.close()
 
@@ -70,5 +119,5 @@ df_movies['title'] = df_movies['title'].apply(lambda x: x.strip())
 # Se elimina la columna timestamp de ratings ya que no es relevante
 df_rate = df_rate.drop('timestamp', 1)
 
-filtrado_usuarios(df_rate, df_movies)
-#filtrado_contenido(df_rate, df_movies)
+#filtrado_usuarios(df_rate, df_movies, 1)
+filtrado_contenido(df_rate, df_movies, 1)
