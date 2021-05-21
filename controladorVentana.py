70bc5f8647e570985ff8aaad917c1f3ba922d313
javirg1005 <55756129+javirg1005@users.noreply.github.com>
2021-05-21 13:38:30 +0200

    mejora controlador ventana

--- controladorVentana.py
+++ controladorVentana.py
@@ -8,7 +8,7 @@ n_pelis = ""
 u_sim = ""
 #Predecir
 pred_usu = ""
-pref_peli = ""
+pred_peli = ""
 
 
 class MyWindow(QtWidgets.QMainWindow):
@@ -27,10 +27,37 @@ class MyWindow(QtWidgets.QMainWindow):
 
     def controlador_predecir():
         print("Predizco")
+    
+    def get_usu(): #Recoge usuario de recomendacion
+        return "usu"
+
+    def get_npelis(): #Recoge numero de pelis a mostrar en la tabla de recomendacion
+        return "npelis"
+
+    def get_u_sim(): #Recoge umbral de similitud de recomendacion
+        return "u_sim"
+
+    def get_pred_usu(): #Recoge el usuario de prediccion
+        return "u_sim"
+    
+    def get_pred_peli(): #Recoge el peli de prediccion
+        return "pred_peli"
+
+    def controlador_comboBox(self):
+        value = str(self.comboBox_Algoritmo.currentText())
+
+        if value == "Naive Bayes":
+                self.txt_Algoritmo.setText("Naive Bayes")
+        elif value == "Árbol de decisión":
+                self.txt_Algoritmo.setText("Árbol de decisión")
+        elif value == "k-NN":
+                self.txt_Algoritmo.setText("k-NN")
+
+
 
 #Método main de la aplicación
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = MyWindow()
-    window.showMaximized()
+    window.show()
     sys.exit(app.exec_())
