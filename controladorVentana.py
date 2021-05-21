e25bf1f76e18f46df92c52eb1965aec4390f745b
javirg1005 <55756129+javirg1005@users.noreply.github.com>
2021-05-21 13:19:06 +0200

    Funcionalidad ventana

--- controladorVentana.py
+++ controladorVentana.py
@@ -2,14 +2,35 @@
 import sys
 from PyQt5 import QtWidgets, uic
 
+#Recomendar
+usuario = ""
+n_pelis = ""
+u_sim = ""
+#Predecir
+pred_usu = ""
+pref_peli = ""
+
+
 class MyWindow(QtWidgets.QMainWindow):
-    
+
     def __init__(self):
         super(MyWindow,self).__init__()
-        uic.loadUi('instagram.ui',self)      
+        uic.loadUi('interfaz.ui',self)      
 
-    if __name__ == '__main__':
-        app = QtWidgets.QApplication(sys.argv)
-        window = MyWindow()
-        window.showMaximized()
-        sys.exit(app.exec_())
+        #Metodos de botones
+        self.btn_reco.clicked.connect(self.controlador_recomendar)
+
+        self.btn_pred.clicked.connect(self.controlador_predecir)
+
+    def controlador_recomendar():
+        print("Recomiendo")
+
+    def controlador_predecir():
+        print("Predizco")
+
+#Método main de la aplicación
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyWindow()
+    window.showMaximized()
+    sys.exit(app.exec_())
